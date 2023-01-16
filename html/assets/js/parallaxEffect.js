78acc908aae036ab169d8ed2fc535901c754279f
kone4580 <yokoyama@i-square.jp>
2023-01-16 13:21:21 +0900

    更新

--- html/assets/js/parallaxEffect.js
+++ html/assets/js/parallaxEffect.js
@@ -1,32 +1,34 @@
-class ParallaxEffectBackground {
-	constructor() {
-		this.devided = 5;
-		this.target = '.title_image';
-		this.setBackgroundPosition();
+if ($(window).width() > 1160) {
+	class ParallaxEffectBackground {
+		constructor() {
+			this.devided = 5;
+			this.target = '.title_image';
+			this.setBackgroundPosition();
+		}
+
+		getScrollTop() {
+			return Math.max(
+				window.pageYOffset,
+				document.documentElement.scrollTop,
+				document.body.scrollTop,
+				window.scrollY
+			);
+		}
+
+		setBackgroundPosition() {
+			document.addEventListener('scroll', e => {
+				const scrollTop = this.getScrollTop();
+				const position = scrollTop / this.devided;
+				if (position) {
+					document.querySelectorAll(this.target).forEach(element => {
+						element.style.backgroundPosition = 'center top -' + position + 'px';
+					});
+				}
+			});
+		}
 	}
 
-	getScrollTop() {
-		return Math.max(
-			window.pageYOffset,
-			document.documentElement.scrollTop,
-			document.body.scrollTop,
-			window.scrollY
-		);
-	}
-
-	setBackgroundPosition() {
-		document.addEventListener('scroll', e => {
-			const scrollTop = this.getScrollTop();
-			const position = scrollTop / this.devided;
-			if (position) {
-				document.querySelectorAll(this.target).forEach(element => {
-					element.style.backgroundPosition = 'center top -' + position + 'px';
-				});
-			}
-		});
-	}
+	document.addEventListener('DOMContentLoaded', event => {
+		new ParallaxEffectBackground();
+	});
 }
-
-document.addEventListener('DOMContentLoaded', event => {
-	new ParallaxEffectBackground();
-});
