a0b0573f5c463a3c837ae9790d46c5714fa5a6a6
kone4580 <yokoyama@i-square.jp>
2023-03-07 19:51:31 +0900

    画像更新

--- html/assets/js/jquery.cmn.js
+++ html/assets/js/jquery.cmn.js
@@ -1,18 +1,13 @@
 $(window).on('load', function() {
 	_pageScroll();
 	_fixedHeader();
-	_preload();
 	faqClick();
-	_modalOpen();
 	if ($(window).width() < 1180) {
 		_setSpMenu();
 	}
 });
 
 
-function _preload(){
-	$('body').removeClass('preload');
-}
 var ifHide = true;
 $(window).scroll(function(){
 	if($(window).scrollTop() < 300 && ifHide==false){
@@ -34,13 +29,13 @@ $(window).scroll(function(){
 
 
 function _fixedHeader(){
-  $(window).on('scroll', function() {
-    if ($(this).scrollTop() > 300) {
-      $('.fixed-header').addClass('fixed');
-    } else {
-      $('.fixed-header').removeClass('fixed');
-    }
-  });
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() > 300) {
+			$('.fixed-header').addClass('fixed');
+		} else {
+			$('.fixed-header').removeClass('fixed');
+		}
+	});
 }
 
 
@@ -49,10 +44,10 @@ function _setSpMenu(){
 		if($("#header").hasClass("opened")) {
 			$("#header").removeClass("opened");
 			$(".menuBtn").removeClass("menu-open");
-    } else {
+		} else {
 			$("#header").addClass("opened");
 			$(".menuBtn").addClass("menu-open");
-    }
+		}
 	});
 }
 
@@ -71,33 +66,14 @@ function _pageScroll(){
 
 
 
-/* タブ切り替え */
-$(function(){
-  $('.tab-target').eq(0).addClass('current');
-
-  $('.tab_navi li').click(function(){
-    var index = $('.tab_navi li').index(this);
-
-    if($(this).hasClass('current')){
-    }else{
-      $('.tab_navi li').removeClass('current');
-      $(this).addClass('current');
-
-      $('.tab-target').removeClass('current');
-      $('.tab-target').eq(index).addClass('current');
-    }
-  });
-});
-
-
 $(window).on('load scroll', function(){
 	$(".animation").each(function(){
-	  var position = $(this).offset().top;
-	  var scroll = $(window).scrollTop();
-	  var windowHeight = $(window).height();
-	  if (scroll > position - windowHeight + windowHeight/4){
+		var position = $(this).offset().top;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll > position - windowHeight){
 			$(this).addClass('moved');
-	  }
+		}
 	});
 });
 
@@ -105,20 +81,5 @@ $(window).on('load scroll', function(){
 function faqClick() {
 	$('.js-accordion__trigger').click(function(){
 		$(this).parents('.js-accordion').toggleClass('active');
-	});
-}
-
-function _modalOpen() {
-	$('.js-modal-open').each(function(){
-		$(this).on('click',function(){
-			var target = $(this).data('target');
-			var modal = document.getElementById(target);
-			$(modal).fadeIn();
-			return false;
-		});
-	});
-	$('.js-modal-close').on('click',function(){
-		$('.js-modal').fadeOut();
-		return false;
 	});
 }
